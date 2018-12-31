44b41db90980685e1e1ac1380dc46475c7687018
Scott Chamberlain <myrmecocystus@gmail.com>
2018-12-31 13:32:56 -0800

    #38 started toying with including() method for partial matching

---
+++ R/including.R
@@ -0,0 +1,18 @@
+#' partially match
+#' 
+#' @export
+#' @param x
+#' @examples
+#' including(list("foo"))
+#' including(list(foo = "bar"))
+#' # just keys by setting values as NULL
+#' including(list(foo = NULL, bar = NULL))
+#' 
+#' # in a stub
+#' req <- stub_request("get", "https://httpbin.org/get")
+#' wi_th(req, body = list(foo = "bar"))
+#' wi_th(req, body = including(list(foo = "bar")))
+including <- function(x) {
+  attr(x, "partial_match") <- TRUE
+  return(x)
+}
